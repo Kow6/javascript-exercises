e379c2bdf0841f78f5b426e2b5e386659709d1c7
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-03 13:24:49 -0500

    Add code for exercise 02 test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+   
+let printthis = string
+
+while (num > 1){
+    printthis += string
+
+
+num--
+}
+
+return printthis
 
 };
+
+repeatString('hey', 3)
 
 // Do not edit below this line
 module.exports = repeatString;
