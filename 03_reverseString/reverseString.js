c38e5a1d296b3ab6fde6e4019d9ffcfe95d705b8
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-03 15:18:59 -0500

    Add code for 03 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,36 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    
+    
+    let printThis = ""
+    
+    let track = input.length
+    
+    while (track > -1){
+          let trackTwo = track + 1
+          let holding = input.slice(track, trackTwo)
+          printThis = printThis + holding
+          track--
+    }
+    return printThis
+}
 
-};
+reverseString('donkey')
+
+
+
+/* doesn't work
+    stringLength = input.length;
+    
+    while(stringLength > 0)
+    {
+        let holding = input.slice(stringLength, stringLength)
+        let printThis = printThis + holding
+
+        stringLength--
+    }
+
+    return printThis
+*/
 
 // Do not edit below this line
 module.exports = reverseString;
