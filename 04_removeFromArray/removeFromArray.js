fa0f04bd8b6b2195439ce0090976170774ab964b
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-05 15:54:49 -0500

    Add code for remove froman array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, toBeRemoved) {
+  
+   let i = myArray.indexOf(toBeRemoved)
+   myArray.splice(i, 1)
+  return myArray
 
-};
-
+  
+    
+  }
+  
+  
+  removeFromArray([1, 2, 3, 4], 3)
 // Do not edit below this line
 module.exports = removeFromArray;
