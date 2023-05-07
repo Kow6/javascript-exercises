602e6477eea61e65de0252a6181b4ecbab60f3c2
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-07 14:32:18 -0500

    Add code for sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
-};
+    if (numOne < 0 || numTwo < 0 || typeof(numOne) != "number" || typeof(numTwo) != "number") {
+      return 'ERROR'}
+      
+    else{
+    
+    const sortArray = [numOne, numTwo]
+    sortArray.sort(function(a, b){return a - b})
+    
+    numTwo = sortArray.pop()
+    numOne = sortArray.pop()
+    
+    
+        let i = numOne
+        let n = 0
+        while (i < numTwo){
+          n = n + i
+          i++}
+        
+           
+        return n + numTwo
+        }
+    }
+        
+       
 
 // Do not edit below this line
 module.exports = sumAll;
