bd8463f1760bd4c7c4db83baf2e1a6df89ef4df2
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-05-08 20:05:08 -0500

    Add code for temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeF) {
+  let convertedC = 5/9 * (degreeF - 32)
+  return Math.round(convertedC * 10) / 10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeC) {
+  let convertedF = (degreeC * 9/5) + 32
+  return Math.round(convertedF * 10) / 10 
 };
 
 // Do not edit below this line
