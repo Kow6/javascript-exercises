cee130340f73d443de25782553880bd7c7360e40
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-06-02 09:15:06 -0500

    Add code through factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,58 @@
-const add = function() {
+const add = function(a,b) {
+  let sum = a + b
+  return sum
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+	let difference = a - b
+  return difference
+};
+
+const sum = function(myArray) {
+
+  let n = 0;
+  let x = 0;
+  
+  while (myArray.length > 0) {
+    
+    n = myArray.pop()
+    x += n;
+  }
+  
+  return x
+   
 	
 };
 
-const sum = function() {
-	
+
+
+const multiply = function(array1) {
+  let n = 0;
+  let x = 1;
+  
+  while (array1.length > 0) {
+    
+    n = array1.pop()
+    x = x * n;
+  }
+  
+  return x
+
 };
 
-const multiply = function() {
+const power = function(a, b) {
+  let root = 1
+  for (let i = 1; i < b + 1; i++){
+    root = root * a
+  }
+return root
 
-};
-
-const power = function() {
 	
 };
 
 const factorial = function() {
+  
 	
 };
 
