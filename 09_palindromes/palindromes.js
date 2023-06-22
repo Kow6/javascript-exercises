f37ebda49dc992f6f46644e36f7001ddfc17824b
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-06-22 10:17:45 -0500

    add code to make palindrones work

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,11 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const noSpaces = word.replace(/[^A-Za-z0-9]/g,'').toLowerCase();
+    const array = noSpaces.split('');
+    const backwardsArray = array.reverse();
+    const backwards = backwardsArray.join('');
+    return noSpaces === backwards ? true : false
+
+
 
 };
 
