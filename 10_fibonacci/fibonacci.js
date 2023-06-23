59775cef0cc1a9ff5016bd610b186a27a6d74f11
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-06-23 05:45:36 -0500

    Add code for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+
+    if (input < 0){return 'OOPS'}
+
+    let a = 1;
+    let b = 1;
+    let c = 0;
+    const array = []
+    
+    while (array.length < Number(input)) {
+    c = a + b
+    array.push(a, b, c)
+    a = c + b
+    b = c + a
+    }
+    
+    return array[(Number(input) -1)]
+    
 
 };
 
