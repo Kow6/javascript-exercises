b229908ce3e667c81f66c49e9345cc1c236f5a0d
Kow6 <31855209+Kow6@users.noreply.github.com>
2023-06-23 09:29:35 -0500

    Add code for find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+const oldest = people.sort((a, b) => {
+    
+    
+
+    let aLifeSpan = a.yearOfDeath - a.yearOfBirth
+    let bLifeSpan = b.yearOfDeath -b.yearOfBirth
+    
+    if (isNaN (aLifeSpan)) {
+        aLifeSpan = new Date().getFullYear() -a.yearOfBirth
+    }
+    if (isNaN (bLifeSpan)) {
+        bLifeSpan = new Date().getFullYear() -b.yearOfBirth
+    }
+
+    return aLifeSpan > bLifeSpan ? -1 : 1}
+    );
+
+return oldest[0]
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
